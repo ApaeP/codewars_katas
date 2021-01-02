9678fafd3fee5f352c0904fde024cea36cdce7d1
Paul Portier <paulportier@gmail.com>
2021-01-02 03:19:59 +0100

    Kata completed (For Twins 2 Math operations 8kyu)

---
+++ 8kyu_for_twins_2_math_operations.rb
@@ -0,0 +1,19 @@
+# Codewars - Kata Solution (Ruby)
+# FOR TWINS 2 MATH OPERATIONS (8 kyu)
+
+# Instructions
+# Task:
+# A magician in the subway showed you a trick, he put an ice brick in a bottle to impress you.The bricks width and height are equal, forming a square. Just for fun and also to impress the magician and people around, you decided to calculate the brick's volume. Write a function iceBrickVolume that will except these parameters:
+# radius - bottle's radius (always > 0);
+# bottleLength - total bottle length (always > 0);
+# rimLength - length from bottle top to brick (always < bottleLength);
+# And return volume of ice brick that magician managed to put into a bottle.
+# Note:
+# All inputs are integers. Assume no irregularities to the cuboid brick. You may assume the bottle is shaped like a cylinder. The brick cannot fit inside the rim.
+# All inputs are integers.
+# Examples:
+# ice_brick_volume(1, 10, 2) # => 16
+# ice_brick_volume(5, 30, 7) # => 1150
+
+# Solution
+
