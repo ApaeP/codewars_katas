ab0a7a9d1bfefbfe739db65a83da307e193bb658
Paul Portier <paulportier@gmail.com>
2021-01-02 05:46:09 +0100

    Kata completed (Detect Pangram 6kyu)

---
+++ 6kyu_detect_pangram.rb
@@ -0,0 +1,11 @@
+# Codewars - Kata Solution (Ruby)
+# DETECT PANGRAM (6 kyu)
+
+# Instructions
+# A pangram is a sentence that contains every single letter of the alphabet at least once. For example, the sentence "The quick brown fox jumps over the lazy dog" is a pangram, because it uses the letters A-Z at least once (case is irrelevant).
+# Given a string, detect whether or not it is a pangram. Return True if it is, False if not. Ignore numbers and punctuation.
+
+# Solution
+def panagram?(string)
+  (('a'..'z').to_a - string.downcase.chars).empty?
+end
