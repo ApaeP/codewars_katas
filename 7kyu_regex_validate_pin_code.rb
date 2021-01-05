a7f6dd58a4448543136b35377d3e19b675589ce9
Paul Portier <paulportier@gmail.com>
2021-01-05 01:52:01 +0100

    Kata completed (Regex validate PIN code 7kyu)

---
+++ 7kyu_regex_validate_pin_code.rb
@@ -0,0 +1,18 @@
+# Codewars - Kata Solution (Ruby)
+# REGEX VALIDATE PIN CODE (7 kyu)
+
+# Instructions
+# ATM machines allow 4 or 6 digit PIN codes and PIN codes cannot contain anything but exactly 4 digits or exactly 6 digits.
+# If the function is passed a valid PIN string, return true, else return false.
+# Examples
+# "1234"   -->  true
+# "12345"  -->  false
+# "a234"   -->  false
+
+# Solution
+def validate_pin(pin)
+  pin.match?(/(\A\d{4}\z|\A\d{6}\z)/)
+end
+
+
+# Completed at : 2021-01-05 01:52:01 +0100
