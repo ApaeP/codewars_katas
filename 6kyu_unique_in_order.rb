cdf32fd09cdbf0a1008bc1f77e6498cba92242d1
Paul Portier <paulportier@gmail.com>
2021-01-05 23:06:44 +0100

    Kata completed (Unique In Order 6kyu)

---
+++ 6kyu_unique_in_order.rb
@@ -0,0 +1,18 @@
+# Codewars - Kata Solution (Ruby)
+# UNIQUE IN ORDER (6 kyu)
+
+# Instructions
+# Implement the function unique_in_order which takes as argument a sequence and returns a list of items without any elements with the same value next to each other and preserving the original order of elements.
+# For example:
+# unique_in_order('AAAABBBCCDAABBB') == ['A', 'B', 'C', 'D', 'A', 'B']
+# unique_in_order('ABBCcAD')         == ['A', 'B', 'C', 'c', 'A', 'D']
+# unique_in_order([1,2,2,3,3])       == [1,2,3]
+
+# Solution
+def unique_in_order(iterable, s = [])
+  pe = (iterable.is_a?(String) ? iterable.chars : iterable).chunk { |x| x }.map(&:first)
+  p pe
+end
+
+
+# Completed at : 2021-01-05 23:06:44 +0100
