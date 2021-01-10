eaacb2a2c61d46833744c7876ba8eec65b8d78b2
Paul Portier <paulportier@gmail.com>
2021-01-10 15:42:16 +0100

    Kata completed (Testing 123 7kyu)

---
+++ Ruby/7kyu_testing_123.rb
@@ -0,0 +1,18 @@
+# Codewars - Kata Solution (Ruby)
+# TESTING 123 (7 kyu)
+
+# Instructions
+# Your team is writing a fancy new text editor and you've been tasked with implementing the line numbering.
+# Write a function which takes a list of strings and returns each line prepended by the correct number.
+# The numbering starts at 1. The format is n: string. Notice the colon and space in between.
+# Examples:
+# number [] # => []
+# number ["a", "b", "c"] # => ["1: a", "2: b", "3: c"]
+
+# Solution
+def number(l)
+  l.map.with_index { |x, i| "#{i+1}: #{x}" }
+end
+
+
+# Completed at : 2021-01-10 15:42:16 +0100
