11b8b30f51660f909604eb68d3c327f14013bb41
Paul Portier <paulportier@gmail.com>
2021-04-01 01:00:45 +0200

    Kata completed (Thinkful  String Drills Repeater 7kyu)

---
+++ Ruby/7kyu_thinkful_string_drills_repeater.rb
@@ -0,0 +1,17 @@
+# Codewars - Kata Solution (Ruby)
+# THINKFUL  STRING DRILLS REPEATER (7 kyu)
+
+# Instructions
+# Write a function named repeater() that takes two arguments (a string and a number), and returns a new string where the input string is repeated that many times.
+# Example:
+# Repeater.repeat("a", 5)
+# should return
+# "aaaaa"
+
+# Solution
+def repeater(string, n)
+  string * n
+end
+
+
+# Completed at : 2021-04-01 01:00:45 +0200
