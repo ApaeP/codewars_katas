cd5caa22159880e7b29ccb53cf272beec9dbd6bc
Paul Portier <paulportier@gmail.com>
2021-04-25 21:17:15 +0200

    Kata completed (Backspaces in string 6kyu)

---
+++ Ruby/6kyu_backspaces_in_string.rb
@@ -0,0 +1,30 @@
+# Codewars - Kata Solution (Ruby)
+# BACKSPACES IN STRING (6 kyu)
+
+# Instructions
+# Assume "#" is like a backspace in string. This means that string "a#bc#d" actually is "bd"
+# Your task is to process a string with "#" symbols.
+# Examples
+# "abc#d##c"      ==>  "ac"
+# "abc##d######"  ==>  ""
+# "#######"       ==>  ""
+# ""              ==>  ""
+
+# Solution
+def clean_string(string, result = [], current_length = 0)
+  string.chars.reverse.select do |e|
+    if e == '#'
+      current_length += 1 and false
+    else
+      if current_length >= 1
+        current_length -= 1 and false
+      else
+        e
+      end
+    end
+  end
+    .reverse.join
+end
+
+
+# Completed at : 2021-04-25 21:17:15 +0200
